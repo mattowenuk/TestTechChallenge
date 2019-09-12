1961908181ecd34d8c6bf98498aa6c742bb36d44
Matt Owen <matt.owen01@bbc.co.uk>
2019-09-12 09:48:13 +0100

    Refactor and add packages to tree

--- app/src/main/java/com/example/TechChallenge/activity/MainActivity.kt
+++ app/src/main/java/com/example/TechChallenge/activity/MainActivity.kt
@@ -1,9 +1,11 @@
-package com.example.dataclasses
+package com.example.TechChallenge.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.dataclasses.request.RequestData
+import com.example.TechChallenge.R
+import com.example.TechChallenge.adapter.DataListAdapter
+import com.example.TechChallenge.request.RequestData
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.*
 
